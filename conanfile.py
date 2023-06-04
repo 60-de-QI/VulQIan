c7c1392b0cb25db45aad95c096434d4fd6205c07
George FRENCH <georgefrench23@gmail.com>
2023-06-04 10:55:57 +0200

    [EVO] added conan-build support

---
+++ conanfile.py
@@ -0,0 +1,16 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+class CompressorRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def requirements(self):
+        self.requires("glfw/3.3.8")
+        self.requires("glm/0.9.5.4")
+
+    def build_requirements(self):
+        self.tool_requires("cmake/3.26.4")
+
+    def layout(self):
+        cmake_layout(self, build_folder="conan-build")
