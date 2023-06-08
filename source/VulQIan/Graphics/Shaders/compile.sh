f69b5bb485f1368d30a1071d8d1ccad7847b2384
George French <george.french@uavia.eu>
2023-06-08 11:10:16 +0200

    [EVO] Added unix based sh for shader compilation

---
+++ source/VulQIan/Graphics/Shaders/compile.sh
@@ -0,0 +1,2 @@
+/home/george/vulkan/1.3.250.0/x86_64/bin/glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
+/home/george/vulkan/1.3.250.0/x86_64/bin/glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
