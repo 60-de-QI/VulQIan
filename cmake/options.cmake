4ee85722d9e07719e06f4c957d033e45cd19ee89
George FRENCH <georgefrench23@gmail.com>
2023-06-16 08:45:10 +0200

    [FIX] Fixing shader compilation step CI

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,1 +1,2 @@
 option(VULQIAN_BUILD_EXAMPLE "Build the VulQIan example" ON)
+option(ENABLE_SHADER_COMPILATION "Build Shaders" ON)
--- source/VulQIan/Shaders/compile.bat
+++ source/VulQIan/Shaders/compile.bat
@@ -1,3 +1,3 @@
-D:\VulkanSDK\1.3.250.0\Bin\glslc.exe simple_shader.vert -o simple_shader.vert.spv
-D:\VulkanSDK\1.3.250.0\Bin\glslc.exe simple_shader.frag -o simple_shader.frag.spv
+D:\VulkanSDK\1.3.250.0\Bin\glslc.exe ./source/VulQIan/Shaders/simple_shader.vert -o ./source/VulQIan/Shaders/simple_shader.vert.spv
+D:\VulkanSDK\1.3.250.0\Bin\glslc.exe ./source/VulQIan/Shaders/simple_shader.frag -o ./source/VulQIan/Shaders/simple_shader.frag.spv
 pause
