4e919c00c56cbdac8c66c35055bae2c52d2920fd
George FRENCH <georgefrench23@gmail.com>
2023-07-16 17:03:33 +0200

    [EVO] enhanced scripts

--- source/VulQIan/Shaders/compile.bat
+++ source/VulQIan/Shaders/compile.bat
@@ -1,3 +1,17 @@
-D:\VulkanSDK\1.3.250.0\Bin\glslc.exe ./source/VulQIan/Shaders/simple_shader.vert -o ./source/VulQIan/Shaders/simple_shader.vert.spv
-D:\VulkanSDK\1.3.250.0\Bin\glslc.exe ./source/VulQIan/Shaders/simple_shader.frag -o ./source/VulQIan/Shaders/simple_shader.frag.spv
+@echo off
+
+set VULKAN_SDK_PATH=D:\VulkanSDK\1.3.250.0
+set GLSLC_EXE=%VULKAN_SDK_PATH%\Bin\glslc.exe
+
+REM Check if glslc.exe exists
+if not exist "%GLSLC_EXE%" (
+  echo Error: glslc.exe not found in %VULKAN_SDK_PATH%
+  exit /b 1
+)
+
+echo glslc.exe found for shader compilation.
+
+"%GLSLC_EXE%" ./source/VulQIan/Shaders/simple_shader.vert -o ./source/VulQIan/Shaders/simple_shader.vert.spv
+"%GLSLC_EXE%" ./source/VulQIan/Shaders/simple_shader.frag -o ./source/VulQIan/Shaders/simple_shader.frag.spv
+
 pause
--- source/VulQIan/Shaders/compile.sh
+++ source/VulQIan/Shaders/compile.sh
@@ -1,2 +1,17 @@
-/home/george/vulkan/1.3.250.0/x86_64/bin/glslc ./source/VulQIan/Shaders/simple_shader.vert -o ./source/VulQIan/Shaders/simple_shader.vert.spv
-/home/george/vulkan/1.3.250.0/x86_64/bin/glslc ./source/VulQIan/Shaders/simple_shader.frag -o ./source/VulQIan/Shaders/simple_shader.frag.spv
+#!/bin/bash
+
+VULKAN_SDK_PATH="/home/george/vulkan/1.3.250.0/x86_64"
+GLSLC_EXE="$VULKAN_SDK_PATH/bin/glslc"
+
+# Check if glslc executable exists
+if [ ! -x "$GLSLC_EXE" ]; then
+  echo "Error: glslc executable not found in $VULKAN_SDK_PATH"
+  exit 1
+fi
+
+echo "glslc found for shader compilation."
+
+"$GLSLC_EXE" ./source/VulQIan/Shaders/simple_shader.vert -o ./source/VulQIan/Shaders/simple_shader.vert.spv
+"$GLSLC_EXE" ./source/VulQIan/Shaders/simple_shader.frag -o ./source/VulQIan/Shaders/simple_shader.frag.spv
+
+read -p "Press Enter to exit"
