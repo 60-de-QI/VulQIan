ad358a10a68fc75644f427d5f30cd4ee3970da34
George FRENCH <george.french@uavia.eu>
2025-07-03 11:58:24 +0200

    fix: 🐛 Updated Conanfile to fix build types (release)

--- conanfile.py
+++ conanfile.py
@@ -1,19 +1,36 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout
 
-class CompressorRecipe(ConanFile):
+
+class VulQIanRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
 
+    # Add options for build configuration
+    options = {
+        "build_tests": [True, False],
+        "build_examples": [True, False]
+    }
+
+    default_options = {
+        "build_tests": True,
+        "build_examples": True
+    }
+
     def configure(self):
-        del self.settings.compiler.libcxx
+        # Remove libcxx setting for C libraries
+        if self.settings.compiler == "gcc":
+            if hasattr(self.settings.compiler, "libcxx"):
+                del self.settings.compiler.libcxx
 
     def requirements(self):
         self.requires("glfw/3.4")
         self.requires("glm/1.0.1")
         self.requires("vulkan-headers/1.4.313.0")
         self.requires("tinyobjloader/2.0.0-rc10")
-        self.requires("gtest/1.16.0")
+
+        if self.options.build_tests:
+            self.requires("gtest/1.16.0")
 
     def build_requirements(self):
         self.tool_requires("cmake/3.26.4")
